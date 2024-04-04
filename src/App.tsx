aba714274da23d29ec1b6ed598602a653f48b41d
Keren <57706914+kerenk100@users.noreply.github.com>
2024-04-04 15:42:00 +0300

    Adding the initial project skeleton
    Adding the initial project skeleton. 
    In addition, adding the components folder with an example structure for adding a new component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+          Baot travel
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/userRegistration/UserRegistration.tsx
@@ -0,0 +1,1 @@
+// TBD just so we will have the components structure
