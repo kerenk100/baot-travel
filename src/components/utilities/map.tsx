050d0c24630810663f9a8def77e50877993cce2b
InbarSle <53119151+InbarSle@users.noreply.github.com>
2024-04-09 19:53:04 +0300

    use env variable for API key

--- src/components/utilities/map.tsx
+++ src/components/utilities/map.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-
 import { APIProvider, Map as GoogleMap, Marker} from '@vis.gl/react-google-maps';
 export interface MapProps{
     lng: number;
     lat: number;
 }
 
-const API_KEY = 'AIzaSyDxrwyU3JroCD11yhG5iBU8bSpKAO4uY0k';
+const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 function Map(props: MapProps) {
     const {lat, lng} = props;
     let location = {
@@ -14,11 +12,11 @@ function Map(props: MapProps) {
     };
 
     return (
-        <APIProvider apiKey={API_KEY}>
+        <APIProvider apiKey={API_KEY ?? ''}>
             <GoogleMap
                 style={{width:'100%', height:'100%'}}
                 defaultZoom={10}
-                center={location}
+                defaultCenter={location}
                 disableDefaultUI={true}>
                 <Marker position={location} />
             </GoogleMap>
