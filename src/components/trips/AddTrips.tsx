dc8aaffe755f226f35292687bdd494f30eff6bdf
Einav Koren <einavk@wix.com>
2024-04-13 21:50:45 +0300

    add state management

--- src/components/trips/AddTrips.tsx
+++ src/components/trips/AddTrips.tsx
@@ -2,31 +2,35 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import { FormControl, FormLabel } from '@mui/material';
-import react, { useState } from 'react';
+import { useState } from 'react';
 
-const [textBudget, setBudget] = useState('');
-const [textStartDate, setStartDate] = useState('');
-const [textEndDate, setEndDate] = useState('');
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 export const AddTrips = () => {
+
+const globalState = {
+  budget: '',
+  startDate: '',
+  endDate: ''
+};
+
+const [state, setState] = useState(globalState);
+
   return (
-    <div className="App">
-      <header className="App-header">
+   
         <FormControl>
           <FormLabel>Enter your name:</FormLabel>
           <TextField />
 
           <div>
           <p>Public post: <Checkbox {...label} defaultChecked /></p>
-          <p>Please enter your budget: <input value={textBudget}   name="budget" type="number" onChange={e => setBudget(e.target.value)} /></p>
-          <p>Please enter your start date: <input value={textStartDate}   name="startDate" type="date" onChange={e => setStartDate(e.target.value)} /></p>
-          <p>Please enter your end date: <input value={textEndDate}   name="endDate" type="date" onChange={e => setEndDate(e.target.value)} /></p>
+          <p>Please enter your budget: <input value={state.budget}   name="budget" type="number" onChange={e => setState({...state, budget: e.target.value})} /></p>
+          <p>Please enter your start date: <input value={state.startDate}   name="startDate" type="date" onChange={e => setState({...state, startDate: e.target.value})} /></p>
+          <p>Please enter your end date: <input value={state.endDate}   name="endDate" type="date" onChange={e => setState({...state, endDate: e.target.value})} /></p>
           </div>
 
           <Button variant="contained">Submit</Button>
         </FormControl>
-      </header>
-    </div>
+
   );
 }
