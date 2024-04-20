0ee407f09ceee4b9ffe104101c5a85e58ac9af0f
maibaron <m.baron@f5.com>
2024-04-20 22:13:04 +0300

    move vendor related cmps to pages Add init header and a footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { AddTrips } from './pages/trips/components/AddTrip/AddTrips.tsx';
-import VendorManager from './VendorManager.tsx';
-import UserRegistration from './components/userRegistration/UserRegistration.tsx';
-import Trips from './pages/trips/Trips.tsx';
-
-
-import EditUser from './components/userEdit/EditUser.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { allRoutes } from "./routes/routes.tsx";
+import { App } from "./App.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: allRoutes,
   },
-  {
-    path:"/trips",
-    element:<Trips />
-  },
-  {
-    path:"/vendors",
-    element:<VendorManager />
-  },
-  {
-    path: "/trips/add-trips",
-    element: <AddTrips />,
-  },
-
-  {
-    path: "/users/register",
-    element: <UserRegistration />
-  },
-  {
-    path: '/users/edit/:userId',
-    element: <EditUser />, 
-  },
-
-
 ]);
 
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ src/pages/index/Index.tsx
@@ -0,0 +1,12 @@
+export const Index = () => {
+  return (
+    <div>
+      <p>Welcome to BaoTravel</p>
+      <p>
+        Here you can find trips suggestions, vendor offers, and everything more
+        for your next trip!
+      </p>
+      <p>Enjoy</p>
+    </div>
+  );
+};
