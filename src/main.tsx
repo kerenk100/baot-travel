48dd1fc6e9679112421d5711d5a750b06e1c661f
ortita <ortaldalman@gmail.com>
2024-04-28 17:26:33 +0300

    Initial table displaying all trips

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,11 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { AddTrips } from './pages/trips/components/AddTrip/AddTrips.tsx';
-import { Trips } from './pages/trips/Trips.tsx';
 import VendorManager from './VendorManager.tsx';
 import UserRegistration from './components/userRegistration/UserRegistration.tsx';
+import Trips from './pages/trips/Trips.tsx';
+
+
 
 const router = createBrowserRouter([
   {
@@ -28,11 +30,14 @@ const router = createBrowserRouter([
     path: "/trips/add-trips",
     element: <AddTrips />,
   },
+
   {
     path: "/users/register",
     element: <UserRegistration />
   }
 ]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/trips/Trips.tsx
+++ src/pages/trips/Trips.tsx
@@ -1,20 +1,62 @@
-import { TripsFeed } from "./components/TripsFeed/TripsFeed";
-export const Trips = () => {
+import { Paper,Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { useEffect, useState } from "react";
+
+export default function BasicTable() {
+  const [trips, setTrips] = useState([]);
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:8080/trips', {
+      method: "GET",
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+    .then(response => response.json()
+    .then(json => setTrips(json)))
+    .finally(()=>{
+      setLoading(false);
+    })
+  },[])
 
   return (
-    <div>
-      <TripsFeed
-        trips={[
-          {
-            country: "US",
-            description: "fun trip",
-            name: "my fun trip to the us",
-            tags: ["lala"],
-            id: "1",
-          },
-        ]}
-        isLoading={false}
-      />
-    </div>
-  );
-};
+    <div className="App">
+    { loading ? (
+        <div>loading...</div>
+        ) : ( 
+          <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+          <TableCell align="right">Title</TableCell>
+            <TableCell align="right">Description</TableCell>
+            <TableCell align="right">Destination</TableCell>
+            <TableCell align="right">Category</TableCell>
+            <TableCell align="right">Start Date</TableCell>
+            <TableCell align="right">End Date</TableCell>
+            <TableCell align="right">Budget</TableCell>
+            <TableCell align="right">Image</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {trips.map((trip) => (
+            <TableRow key={trip} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component="th" scope="row">{trip.title}</TableCell>
+              <TableCell align="right">{trip.description}</TableCell>
+              <TableCell align="right">{trip.destination}</TableCell>
+              <TableCell align="right">{trip.category}</TableCell>
+              <TableCell align="right">{trip.startDate}</TableCell>
+              <TableCell align="right">{trip.endDate}</TableCell>
+              <TableCell align="right">{trip.budget}</TableCell>
+              <TableCell align="right">{trip.image}</TableCell>
+               
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )}
+  </div>
+  )
+}
