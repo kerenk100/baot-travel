c8ab51cc0d244d510dfbecacb3206813895408ec
ortita <ortaldalman@gmail.com>
2024-04-30 13:57:25 +0300

    Refactoring

--- src/pages/trips/Trips.tsx
+++ src/pages/trips/Trips.tsx
@@ -18,9 +18,11 @@ import Tooltip from "@mui/material/Tooltip";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import FilterListIcon from "@mui/icons-material/FilterList";
 import { visuallyHidden } from "@mui/utils";
 import { useEffect, useState } from "react";
+import { Order, getComparator, stableSort } from "../../components/utilities/sortUtils";
 
 interface Trip {
   title: string;
@@ -32,51 +34,6 @@ interface Trip {
   budget: number;
   destination: string;
   id: string;
-}
-
-
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = "asc" | "desc";
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string }
-) => number {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
-function stableSort<T>(
-  array: readonly T[],
-  comparator: (a: T, b: T) => number
-) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
 }
 
 interface HeadCell {
@@ -136,6 +93,7 @@ const headCells: readonly HeadCell[] = [
     label: "Image",
   },
 ];
+
 
 interface EnhancedTableProps {
   numSelected: number;
@@ -245,11 +203,19 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
         </Typography>
       )}
       {numSelected > 0 ? (
+        <>
+        <Tooltip title="Edit">
+          <IconButton>
+            <EditIcon />
+           </IconButton>
+        </Tooltip>
         <Tooltip title="Delete">
           <IconButton>
             <DeleteIcon />
           </IconButton>
         </Tooltip>
+        
+        </>
       ) : (
         <Tooltip title="Filter list">
           <IconButton>
