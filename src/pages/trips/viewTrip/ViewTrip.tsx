d8db7a9aea555604c1cdac8d3ac6bdfb6cb1ae41
InbarSle <53119151+InbarSle@users.noreply.github.com>
2024-05-15 18:29:51 +0300

    Add Image to view trip page

--- src/pages/trips/viewTrip/ViewTrip.tsx
+++ src/pages/trips/viewTrip/ViewTrip.tsx
@@ -2,6 +2,13 @@ import { Box, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Trip } from "../types";
+import { AdvancedImage, placeholder, responsive } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen/index";
+
+const uwConfig = {
+  cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || '',
+  uploadPreset: import.meta.env.VITE_CLOUDINARY_PRESET || ''
+}
 
 export default function ViewTrip() {
   const [loading, setLoading] = useState(true);
@@ -11,6 +18,12 @@ export default function ViewTrip() {
 
   const boxSx = { margin: '10px' };
 
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: uwConfig.cloudName
+    }
+  });
+  
   useEffect(() => {
     setLoading(true)
     fetch(`http://localhost:8080/trips/${tripId}`, {
@@ -26,6 +39,7 @@ export default function ViewTrip() {
       })
   }, [])
 
+  const image = cld.image(trip?.image);
 
   return (
     <div>
@@ -61,6 +75,14 @@ export default function ViewTrip() {
         <Box sx={boxSx}>
           <Typography variant="h6">End date:</Typography>
           <Typography>{trip?.endDate}</Typography>
+        </Box>
+        <Box sx={boxSx}>
+          <Typography variant="h6">Pictures:</Typography>
+          <AdvancedImage
+            style={{ maxWidth: "100px" }}
+            cldImg={image}
+            plugins={[responsive(), placeholder()]}
+          />
         </Box>
       </header>
     </div>)
