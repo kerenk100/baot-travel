0daa0817adc332277618152f11d6a6e15a939a4a
maibaron <m.baron@f5.com>
2024-06-16 20:57:03 +0300

    undo env var change name

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { Response } from "express";
 
 const generateToken = (res: Response, userId: string) => {
-  const jwtSecret = process.env.VITE_JWT_SECRET || "";
+  const jwtSecret = process.env.JWT_SECRET || "";
   const token = jwt.sign({ userId }, jwtSecret, {
     expiresIn: "1h",
   });
