06092e5b6ef7251122f244fcb7b4892a6012d7f0
rina <rinafine94@gmail.com>
2024-06-17 13:55:09 +0300

    combine viewTrip with weather & tripMap

--- src/pages/trips/viewTrip/ViewTrip.tsx
+++ src/pages/trips/viewTrip/ViewTrip.tsx
@@ -4,25 +4,42 @@ import { useParams } from "react-router-dom";
 import { Trip } from "../types";
 import { AdvancedImage, placeholder, responsive } from "@cloudinary/react";
 import { Cloudinary } from "@cloudinary/url-gen/index";
+import { APIProvider } from "@vis.gl/react-google-maps";
+import TripMap from "../components/TripMap/TripMap";
+import styles from "./ViewTrip.module.scss"
+import Weather from "../../../components/utilities/Weather/Weather";
+import { Country } from "country-state-city";
+import axios from "axios";
 
 const uwConfig = {
   cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME || '',
   uploadPreset: import.meta.env.VITE_CLOUDINARY_PRESET || ''
 }
 
+
+
 export default function ViewTrip() {
   const [loading, setLoading] = useState(true);
   const [trip, setTrip] = useState<Trip | undefined>(undefined);
+  const [city, setCity] = useState('');
 
   const { tripId } = useParams();
-
   const boxSx = { margin: '10px' };
-
   const cld = new Cloudinary({
     cloud: {
       cloudName: uwConfig.cloudName
     }
   });
+
+  const defineCityforWeather = (countryCode:string) =>{
+    const countryName = Country.getCountryByCode(countryCode)?.name;
+    if (countryName) { 
+        axios.get(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then(countryJSON => {
+          if (countryJSON) setCity(countryJSON.data[0].capital[0]);
+        });
+    } 
+  }
   
   useEffect(() => {
     setLoading(true)
@@ -33,17 +50,20 @@ export default function ViewTrip() {
       }
     })
       .then(response => response.json()
-        .then(json => setTrip(json)))
+        .then(json => {
+          setTrip(json);
+          if (json.country) defineCityforWeather(json.country);
+      }))
       .finally(() => {
         setLoading(false);
       })
-  }, [])
+  }, [trip])
 
   const image = cld.image(trip?.image);
 
   return (
-    <div>
-      <header className="App-header">
+    <div className={styles.flexContainer}>
+      <div className={styles.flexItem}>
         <Box sx={boxSx}>
           <Typography variant="h6">Trip name:</Typography>
           <Typography>{trip?.title}</Typography>
@@ -84,6 +104,14 @@ export default function ViewTrip() {
             plugins={[responsive(), placeholder()]}
           />
         </Box>
-      </header>
+      </div>
+      <div className={styles.flexItem}>
+          <Weather city={city || ''}/>
+        </div>
+      <div className={styles.flexItem}>
+        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_API_KEY} >
+          <TripMap ></TripMap>
+        </APIProvider>
+      </div>
     </div>)
 } 
